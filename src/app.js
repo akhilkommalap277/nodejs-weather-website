109198850da01159421ab8d66b7691a31fca9b88
Akhil Kommalapati <akhilkommalapati@Akhils-MacBook-Pro.local>
2020-05-03 19:39:51 -0500

    Setting up app for heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const port = process.env.PORT || 3000
 
 const app = express()
 
@@ -72,12 +73,7 @@ app.get('/weather', (req, res) => {
 
     })
 
-    // console.log(req.query.address)
-    // res.send({
-    //     forecast: 'It is snowing',
-    //     location: 'Philadelphia',
-    //     address: req.query.address
-    // })
+    
 })
 
 
@@ -112,6 +108,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port ' + port )
 })
